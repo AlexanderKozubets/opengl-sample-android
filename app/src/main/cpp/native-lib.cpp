096f31274c62a35a5820faa2ce39f680daf540bb
Alexander <kozubets.alexander@gmail.com>
2017-10-16 22:51:36 +0300

    Configure native library

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,15 +1,30 @@
 #include <jni.h>
-#include <string>
 
-extern "C"
+#include <GLES2/gl2.h>
+#include <GLES2/gl2ext.h>
+
+bool setup(int w, int h) {
+    return true;
+}
+
+void renderFrame() {
+}
+
+extern "C" {
+JNIEXPORT void JNICALL Java_com_alexander_kozubets_opengl_view_NativeRenderer_init(
+        JNIEnv *env, jclass type, jint width, jint height);
+
+JNIEXPORT void JNICALL Java_com_alexander_kozubets_opengl_view_NativeRenderer_draw(
+        JNIEnv *env, jclass type);
+};
+
 JNIEXPORT void JNICALL
 Java_com_alexander_kozubets_opengl_view_NativeRenderer_init(JNIEnv *env, jclass type, jint width,
                                                             jint height) {
-    // TODO
+    setup(width, height);
 }
 
-extern "C"
 JNIEXPORT void JNICALL
 Java_com_alexander_kozubets_opengl_view_NativeRenderer_draw(JNIEnv *env, jclass type) {
-    // TODO
+    renderFrame();
 }
