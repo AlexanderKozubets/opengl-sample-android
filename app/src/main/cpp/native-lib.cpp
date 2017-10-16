54a3b15fcd4a8192a94ec792bfb0b5b438b86d53
Alexander <kozubets.alexander@gmail.com>
2017-10-17 00:18:11 +0300

    Draw triangle sample

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -16,6 +16,18 @@ static void checkGlError(const char* operation) {
     }
 }
 
+auto gVertexShader =
+        "attribute vec4 vPosition;\n"
+                "void main() {\n"
+                "  gl_Position = vPosition;\n"
+                "}\n";
+
+auto gFragmentShader =
+        "precision mediump float;\n"
+                "void main() {\n"
+                "  gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);\n"
+                "}\n";
+
 GLuint loadShader(GLenum shaderType, const char* pSource) {
     GLuint shader = glCreateShader(shaderType);
     if (shader) {
@@ -80,11 +92,25 @@ GLuint createProgram(const char* pVertexSource, const char* pFragmentSource) {
     return program;
 }
 
+GLuint gProgram;
+GLuint gvPositionHandle;
+
 bool setupGraphics(int w, int h) {
     printGlValue("Version", GL_VERSION);
     printGlValue("Vendor", GL_VENDOR);
     printGlValue("Renderer", GL_RENDERER);
     printGlValue("Extensions", GL_EXTENSIONS);
+
+    LOGI("setupGraphics(%d, %d)", w, h);
+    gProgram = createProgram(gVertexShader, gFragmentShader);
+    if (!gProgram) {
+        LOGE("Could not create program.");
+        return false;
+    }
+    gvPositionHandle = glGetAttribLocation(gProgram, "vPosition");
+    checkGlError("glGetAttribLocation");
+    LOGI("glGetAttribLocation(\"vPosition\") = %d\n",
+         gvPositionHandle);
 
     glViewport(0, 0, w, h);
     checkGlError("glViewport");
@@ -96,6 +122,21 @@ void renderFrame() {
     checkGlError("glClearColor");
     glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
     checkGlError("glClear");
+
+    glUseProgram(gProgram);
+    checkGlError("glUseProgram");
+
+    static const GLfloat gTriangleVertices[] = {
+             0.0f,  1.0f, // top vertex
+            -1.0f, -1.0f, // left bottom vertex
+             1.0f, -1.0f  // right bottom vertex
+    };
+    glVertexAttribPointer(gvPositionHandle, 2, GL_FLOAT, GL_FALSE, 0, gTriangleVertices);
+    checkGlError("glVertexAttribPointer");
+    glEnableVertexAttribArray(gvPositionHandle);
+    checkGlError("glEnableVertexAttribArray");
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    checkGlError("glDrawArrays");
 }
 
 extern "C" {
