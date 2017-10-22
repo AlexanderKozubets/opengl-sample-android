674cc9930e54e4c4d90a0fc85e75a610197fc398
Alexander <kozubets.alexander@gmail.com>
2017-10-22 15:56:00 +0300

    Draw fullscreen rectangle

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -127,15 +127,19 @@ void renderFrame() {
     checkGlError("glUseProgram");
 
     static const GLfloat gTriangleVertices[] = {
-            -1.0f,  1.0f, // left top vertex
-            -1.0f, -1.0f, // left bottom vertex
-             1.0f, -1.0f  // right bottom vertex
+            -1.0f,  1.0f, 0.0f, // left top vertex,     0 index
+            -1.0f, -1.0f, 0.0f, // left bottom vertex,  1 index
+             1.0f, -1.0f, 0.0f, // right bottom vertex, 2 index
+             1.0f,  1.0f, 0.0f  // right top vertex,    3 index
     };
-    glVertexAttribPointer(gvPositionHandle, 2, GL_FLOAT, GL_FALSE, 0, gTriangleVertices);
+
+    const GLubyte indices[] = { 0, 1, 2, 2, 3, 0};
+
+    glVertexAttribPointer(gvPositionHandle, 3, GL_FLOAT, GL_FALSE, 0, gTriangleVertices);
     checkGlError("glVertexAttribPointer");
     glEnableVertexAttribArray(gvPositionHandle);
     checkGlError("glEnableVertexAttribArray");
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(GLubyte), GL_UNSIGNED_BYTE, indices);
     checkGlError("glDrawArrays");
 }
 
