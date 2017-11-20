58f00b27d75c60bd104154922f8023270336fea6
Alexander <kozubets.alexander@gmail.com>
2017-11-20 22:59:41 +0200

    Add interface for scene

--- app/src/main/cpp/IScene.h
+++ app/src/main/cpp/IScene.h
@@ -1,8 +1,13 @@
 //
-// Created by Sasha on 11/20/17.
+// Created by Alexander Kozubets on 11/20/17.
 //
 
-#ifndef OPENGL_SAMPLE_ANDROID_SCENE_H
-#define OPENGL_SAMPLE_ANDROID_SCENE_H
+#ifndef OPENGL_SAMPLE_ANDROID_ISCENE_H
+#define OPENGL_SAMPLE_ANDROID_ISCENE_H
 
-#endif //OPENGL_SAMPLE_ANDROID_SCENE_H
+class IScene {
+public:
+    virtual void draw() = 0;
+};
+
+#endif //OPENGL_SAMPLE_ANDROID_ISCENE_H
