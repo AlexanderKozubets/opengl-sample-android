7c60dd46d305064f82f4f2c6044577dbf081df1d
Alexander <kozubets.alexander@gmail.com>
2018-01-14 03:40:37 +0200

    Implement matrix multiplication

--- app/src/main/cpp/math/matr4.h
+++ app/src/main/cpp/math/matr4.h
@@ -21,7 +21,7 @@ public:
     ~matr4() {
     }
 
-    float *ptr() {
+    const float * ptr() const {
         return m;
     }
 
@@ -102,6 +102,35 @@ public:
                         0, 0, 1, 0,
                         0, 0, 0, 1
                 });
+    }
+
+    // friends defined inside class body are inline and are hidden from non-ADL lookup
+    friend matr4 operator*(matr4 lhs, const matr4 &rhs) {
+        float* m = new float[16]{0};
+        const float *l = lhs.ptr();
+        const float *r = rhs.ptr();
+
+        m[0] = l[0] * r[0] + l[1] * r[4] + l[2] * r[8] + l[3] * r[12];
+        m[1] = l[0] * r[1] + l[1] * r[5] + l[2] * r[9] + l[3] * r[13];
+        m[2] = l[0] * r[2] + l[1] * r[6] + l[2] * r[10] + l[3] * r[14];
+        m[3] = l[0] * r[3] + l[1] * r[7] + l[2] * r[11] + l[3] * r[15];
+
+        m[4] = l[4] * r[0] + l[5] * r[4] + l[6] * r[8] + l[7] * r[12];
+        m[5] = l[4] * r[1] + l[5] * r[5] + l[6] * r[9] + l[7] * r[13];
+        m[6] = l[4] * r[2] + l[5] * r[6] + l[6] * r[10] + l[7] * r[14];
+        m[7] = l[4] * r[3] + l[5] * r[7] + l[6] * r[11] + l[7] * r[15];
+
+        m[8] = l[8] * r[0] + l[9] * r[4] + l[10] * r[8] + l[11] * r[12];
+        m[9] = l[8] * r[1] + l[9] * r[5] + l[10] * r[9] + l[11] * r[13];
+        m[10] = l[8] * r[2] + l[9] * r[6] + l[10] * r[10] + l[11] * r[14];
+        m[11] = l[8] * r[3] + l[9] * r[7] + l[10] * r[11] + l[11] * r[15];
+
+        m[12] = l[12] * r[0] + l[13] * r[4] + l[14] * r[8] + l[15] * r[12];
+        m[13] = l[12] * r[1] + l[13] * r[5] + l[14] * r[9] + l[15] * r[13];
+        m[14] = l[12] * r[2] + l[13] * r[6] + l[14] * r[10] + l[15] * r[14];
+        m[15] = l[12] * r[3] + l[13] * r[7] + l[14] * r[11] + l[15] * r[15];
+
+        return matr4(m); // return the result by value (uses move constructor)
     }
 };
 
